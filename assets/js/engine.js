1c5ee1c5d8de8fd3b3ab838adab79ce2e73a49c1
davidosongschool <60469218+davidosongschool@users.noreply.github.com>
2020-11-17 13:54:38 +0000

    colour change

--- assets/js/engine.js
+++ assets/js/engine.js
@@ -3,6 +3,11 @@
 let shopsfound;
 let numofshops;
 
+let munster = ["Clare", "Cork", "Kerry", "Limerick", "Tipperary", "Waterford"];
+let leinster = ["Carlow", "Dublin", "Kildare", "Kilkenny", "Laois", "Longford", "Louth", "Meath", "Offaly", "Westmeath", "Wexford", "Wicklow"];
+let ulster = ["Donegal"];
+let connacht = ["Galway", "Leitrim", "Mayo", "Roscommon", "Sligo"];
+
 function selectCounty(county) {
 $(".contain-salon").addClass('d-none');
 $("." + county).removeClass('d-none');
@@ -92,7 +97,48 @@ function displayData(data, county) {
     }
 
     if (shopsfound != 1){
-        document.getElementById("container-row").innerHTML = '<div class="col-12 text-center">Looks like there are no shops in your county yet!</div><div class="col-12 mt-4 text-center"><h4>Check out shops in your region!</h4></div><div class="col-12 mt-3 mb-5 pb-5"> <div class="select-county-btn-desktop text-center" data-toggle="modal" data-target="#provModal">Select A Region <i class="fas fa-angle-down ml-3"></i></div></div>';
+        document.getElementById("container-row").innerHTML = '<div class="col-12 text-center">Looks like there are no shops in your county yet!</div><div class="col-12 mt-4 text-center"><h4>Check out shops in your region!</h4></div>';
+        
+        // GET PROVINCE COUNTY IS IN
+        
+
+        if (county == 'Clare') {
+            area = "Munster";
+        }
+       
+        // OUTPUT PROVINCE DATA IF NO SHOP IN COUNTY
+
+        // GET DATA //
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                displayData(JSON.parse(this.responseText), area);
+
+            }
+        };
+        
+        xhr.open("GET", "assets/database/data.json");
+        xhr.send();
+
+        function displayData(data, area) {
+            console.log("function called");
+            shop = data;
+            
+            document.getElementById("resultstotal").innerHTML = '';
+            shopsfound = 0;
+            numofshops = 0;
+
+            for (i = 0; i < shop.shops.length; i++) {
+                if (shop.shops[i].Area.includes(area)) {
+                    numofshops++;
+                    shopsfound = 1;
+                    document.getElementById("container-row").innerHTML += '<div class="col-12 col-md-6 col-xl-4 contain-salon mt-3 dublin"><a href="' + shop.shops[i].Link + '"><img src="' + shop.shops[i].Image + '" alt="' + shop.shops[i].Name + '" class="screenshot shadow-sm"></a><div class="contain-info mt-3 d-inline-block p-0">' + shop.shops[i].Name + '</div><div class="mt-3 contain-location d-inline-block p-0"><div class="county-tag ml-auto">' + shop.shops[i].County + '</div></span></div></div>'
+                }
+            }
+
+        }
+
+
     }
 
 
@@ -112,7 +158,7 @@ xhr.onreadystatechange = function() {
 
     }
 };
-// set url based on selected trending item
+
 xhr.open("GET", "assets/database/data.json");
 xhr.send();
     
